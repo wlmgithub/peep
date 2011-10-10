37d1674e68349fd10c229b265ee038098781e8bb
Liming Wang <liming@twitter.com>
2011-10-10 13:39:01 -0700

    new

---
+++ scala/topvotes.scala
@@ -0,0 +1,34 @@
+// http://prezi.com/ewvgkgw5pgim/scala-for-java-programmers/
+
+/*
+
+ $ scala -version
+ Scala code runner version 2.7.7.final -- Copyright 2002-2009, LAMP/EPFL
+
+ */
+
+import scala.collection.mutable.HashMap
+
+def main(args:Array[String]) {
+  val votes = List(
+          ("Bob", 13),
+          ("Ann", 3),
+          ("Maria", 18)
+          )
+  val topVotes = votes.filter{
+      case(name, num) => num > 5
+  }
+
+  val hm = new HashMap[String, Int]
+  topVotes.foreach {
+    case(name, num) => hm.put(name,num)
+  }
+
+  println(hm.toString)
+  hm.foreach {
+    case(name, num) => println(name + " => " + num)
+  }
+
+}
+
+main(args)
