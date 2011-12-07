273f0ba0688a42e88f5f63bc9c537e129d310fa7
Liming <liming@twitter.com>
2011-12-07 10:52:58 -0800

    new

---
+++ lang/ruby/use_jira4r.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'jira4r'
+require 'highline/import'
+
+def get_password(prompt="Enter Password")
+   ask(prompt) {|q| q.echo = false}
+end
+
+if ARGV.size != 1
+  puts "usage: script <username> "
+  exit
+end
+
+username = ARGV[0]
+password = get_password()
+
+jira = Jira4R::JiraTool.new(2, "http://jira.local.twitter.com")
+jira.login(username, password)
+
+p jira.getProjectByKey('BUILD')
+
+
+exit
+
+
