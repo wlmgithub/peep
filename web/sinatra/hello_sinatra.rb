4712996cf15d0c9979d3ba3a0d36a742936f4918
Liming Wang <liming@twitter.com>
2012-11-13 10:24:04 -0800

    new

---
+++ web/sinatra/hello_sinatra.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+#
+# http://ruby.about.com/od/sinatra/ss/sinatra4_7.htm
+#
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+ 
+get '/' do
+  haml :index
+end
+ 
+# This one shows how you can use refer to
+# variables in your Haml views.
+# This method uses member variables.
+get '/hello/:name' do|name|
+  @name = name
+  haml :hello
+end
+ 
+# This method shows you how to inject
+# local variables
+get '/goodbye/:name' do|name|
+  haml :goodbye, :locals => { :name => name }
+end
+ 
+__END__
+@@ layout
+%html
+  %head
+    %title Haml on Sinatra Example
+  %body
+    =yield
+ 
+@@ index
+#header
+  %h1 Haml on Sinatra Example
+#content
+  %p
+    This is an example of using Haml on Sinatra.
+    You can use Haml in all your projeccts now, instead
+    of Erb. I'm sure you'll find it much easier!
+ 
+@@ hello
+%h1= "Hello #{@name}!"
+ 
+@@ goodbye
+%h1= "Goodbye #{name}!"
